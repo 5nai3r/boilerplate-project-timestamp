06e8fbe8aaaa6135236ddc2847749a6111e2445d
ismaildevv@gmail.com <ismaildevv@gmail.com>
2023-03-10 17:10:41 +0100

    /api endpoint

--- index.js
+++ index.js
@@ -19,14 +19,28 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+let utcRes, unixRes;
+
+app.get("/api/", function (req, res) {
+  const dateObj = new Date();
+  unixRes = dateObj.getTime();
+  utcRes = dateObj.toUTCString()
+
+  res.json({
+    unix: unixRes,
+    utc: utcRes
+  }
+  );
+});
+app.get("/api/:data", function (req, res) {
+  res.json({
+    name :'dsds'
+  });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3232, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
